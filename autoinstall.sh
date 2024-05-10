a2101d0272fc44c964f8f287f0ac415037742234
Prismey <prismey@murena.io>
2024-05-10 17:07:48 +0200

    changed gitignore and configured autoinstall to set the prompt

--- autoinstall.sh
+++ autoinstall.sh
@@ -12,3 +12,6 @@ sudo pacman -S fisher\
 # fish config
 echo "Installing plugins with fisher"
 fisher install $(cat ./.config/fish/fish_plugins)
+
+echo "Configuring fish prompt"
+tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time=No --rainbow_prompt_separators=Round --powerline_prompt_heads=Round --powerline_prompt_tails=Flat --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Solid --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Dark --prompt_spacing=Sparse --icons='Many icons' --transient=No
