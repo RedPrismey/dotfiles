70240284fc8bcf2d2f25989caf7debed3d28fd65
Prismey <prismey@murena.io>
2024-05-13 08:24:10 +0200

    changed prompt and fixed gitignore

--- autoinstall.sh
+++ autoinstall.sh
@@ -4,14 +4,11 @@
 
 # All required packages
 echo "Installing packages using pacman"
-sudo pacman -S fisher\
-  alacritty\
-  neovim\
-  hyprland hyprpaper waybar
+sudo pacman -S fisher alacritty neovim hyprland hyprpaper waybar
 
 # fish config
 echo "Installing plugins with fisher"
 fisher install $(cat ./.config/fish/fish_plugins)
 
 echo "Configuring fish prompt"
-tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time=No --rainbow_prompt_separators=Round --powerline_prompt_heads=Round --powerline_prompt_tails=Flat --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Solid --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Dark --prompt_spacing=Sparse --icons='Many icons' --transient=No
+tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time=No --rainbow_prompt_separators=Round --powerline_prompt_heads=Round--powerline_prompt_tails=Round --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Solid --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Light --prompt_spacing=Sparse --icons='Few icons' --transient=Yes
