9037dbb2ae692c18e293437a233fc90ab4a934f3
Prismey <prismey@murena.io>
2024-05-27 15:13:21 +0200

    fixed autoinstall and added better explaination

---
+++ README.md
@@ -0,0 +1,17 @@
+My personal dotfiles repo.
+
+Feel free to use and tweek my config to your liking.
+
+Things I use : 
+- `fish` 
+  - `fisher`
+  - `starship`
+- `alacritty`
+- `neovim`
+- `hyprland`
+  - `hyprpaper`
+  - `waybar`
+- `bat`
+- `zoxide`
+
+If you are on arch, you can launch `autoinstall.sh` with fish, it will install all required packages, install fish plugins and ask you for a variable that is used in an abbreviation 
--- autoinstall.sh
+++ autoinstall.sh
@@ -4,17 +4,12 @@
 
 # All required packages
 echo "Installing packages using pacman"
-sudo pacman -S fisher alacritty neovim hyprland hyprpaper waybar bat zoxide
+sudo pacman -S fisher alacritty neovim hyprland hyprpaper waybar bat zoxide starship
 
 
 # fish config
 echo "Installing plugins with fisher"
 fisher install $(cat ./.config/fish/fish_plugins)
-
-
-echo "Configuring fish prompt"
-tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time=No --rainbow_prompt_separators=Round --powerline_prompt_heads=Round --powerline_prompt_tails=Round --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Solid --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Light --prompt_spacing=Sparse --icons='Few icons' --transient=Yes
-
 
 echo "Configuring fish variables"
 set -Ux EDITOR nvim
