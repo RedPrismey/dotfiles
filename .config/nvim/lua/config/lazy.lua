9665f029f4b5cf54f1c7f456367631941bd34090
Prismey <prismey@murena.io>
2025-06-05 16:40:01 +0200

    changed typst commands and removed useless files

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -22,7 +22,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
 
     { import = "lazyvim.plugins.extras.lang.rust" },
-    { import = "lazyvim.plugins.extras.lang.toml" },
 
     { import = "lazyvim.plugins.extras.lang.clangd" },
 
